6fe8ca80782a4230edcf8a30ec9972579eb23720
Damoit455 <Gamingstyles159@gmail.com>
2017-02-08 09:35:47 +0100

    delete taffydb

---
+++ LetsGoOut/LetsGoOut/Javascript/CreateEvent.js
@@ -0,0 +1,7 @@
+﻿// Create DB and fill it with records
+var friends = TAFFY([
+	{ "id": 1, "gender": "M", "first": "John", "last": "Smith", "city": "Seattle, WA", "status": "Active" },
+	{ "id": 2, "gender": "F", "first": "Kelly", "last": "Ruth", "city": "Dallas, TX", "status": "Active" },
+	{ "id": 3, "gender": "M", "first": "Jeff", "last": "Stevenson", "city": "Washington, D.C.", "status": "Active" },
+	{ "id": 4, "gender": "F", "first": "Jennifer", "last": "Gill", "city": "Seattle, WA", "status": "Active" }
+]);
